e7b339596cfdd5f57db8e9947a118cc1744f700c
Rixuel <rixuel@hotmail.com>
2021-03-13 02:48:46 -0500

    WIP: adding 2nd boss and toggling feature

--- ma/js/marena.js
+++ ma/js/marena.js
@@ -25,10 +25,15 @@ var valkimgs = [
         "img": "https://uploadstatic-sea.mihoyo.com/contentweb/20200618/2020061816132847384.png"
     },
     {
+        "acronym": ["SS"],
+        "img": "https://uploadstatic-sea.mihoyo.com/contentweb/20200619/2020061915594971535.png"
+    },
+    {
         "acronym": ["TP", "VE"],
         "img": "https://uploadstatic-sea.mihoyo.com/contentweb/20200721/2020072117425786448.png"
     }
 ]
+
 
 
 var MABosses = [
@@ -73,19 +78,58 @@ var MABosses = [
                 "video": "https://youtu.be/j5eJOZoVBdE?t=74"
             }
         ]
+    },
+    {
+        "name": "Huodou",
+        "type": "Biologic",
+        "team": [
+            {
+                "party": [
+                    {
+                        "valk": "SS",
+                        "rank": "SSS"
+                    },
+                    {
+                        "valk": "DK",
+                        "rank": "SSS"
+                    },
+                    {
+                        "valk": "TP",
+                        "rank": "SSS"
+                    }
+                ],
+                "player": "Marisa Honkai",
+                "video": "https://youtu.be/b5z0C2YHu90?t=27"
+            }
+        ]
     }
 ]
+var MABossesLength = MABosses.length;
+console.log("MABossesLength: " + MABossesLength);
+var bossIndex = "";
 
-function MABossInfo() {
+function toggleDisplay(bossName) {
+    var x = document.getElementById(bossName);
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+
+    MABossInfo(bossName);
+}
+
+function MABossInfo(bossName) {
     var cardInfo = "";
 
-    cardInfo += "<h1 class='title is-4'>" + MABosses[0].name + "</h1>";
-    cardInfo += "<h6 class='subtitle is-6'>" + MABosses[0].type + "</h6>";
-    cardInfo += "<p>Teams</p>";
-    cardInfo += "TEST: " + MABosses[0].team.length;
+    findBossIndex(bossName);
+
+    cardInfo += "<h6 class='subtitle is-6'>" + MABosses[bossIndex].type + "</h6>";
+    cardInfo += "<p>Number of Teams: " + MABosses[bossIndex].team.length + "</p>";
     cardInfo += teamSetup();
 
-    document.getElementById("Rimestar").innerHTML = cardInfo;
+    document.getElementById(bossName).innerHTML = cardInfo;
 }
 
 function teamSetup() {
@@ -97,41 +141,52 @@ function teamSetup() {
     var img3 = "";
     var numberOfTeams = 0;
 
-    numberOfTeams = MABosses[0].team.length;
+    numberOfTeams = MABosses[bossIndex].team.length;
     for (i=0; numberOfTeams>i; i++) {
         party += "<div class='team-style'>";
             party += "<table>";
                 party += "<tr>";
-                    party += "<td class='label-style' align='right'>" + MABosses[0].team[i].party[0].valk + "</td>";
-                    party += "<td class='label-style' align='right'>" + MABosses[0].team[i].party[1].valk + "</td>";
-                    party += "<td class='label-style' align='right'>" + MABosses[0].team[i].party[2].valk + "</td>";
+                    party += "<td class='label-style' align='right'>" + MABosses[bossIndex].team[i].party[0].valk + "</td>";
+                    party += "<td class='label-style' align='right'>" + MABosses[bossIndex].team[i].party[1].valk + "</td>";
+                    party += "<td class='label-style' align='right'>" + MABosses[bossIndex].team[i].party[2].valk + "</td>";
                 party += "</tr>";
 
                 // Search in the pool of images
                 for (j=0; valkimgs.length>j; j++) {
-                    if (valkimgs[j].acronym.includes(MABosses[0].team[i].party[0].valk)) {
+                    if (valkimgs[j].acronym.includes(MABosses[bossIndex].team[i].party[0].valk)) {
                         img1 = valkimgs[j].img;
                     }
-                    if (valkimgs[j].acronym.includes(MABosses[0].team[i].party[1].valk)) {
+                    if (valkimgs[j].acronym.includes(MABosses[bossIndex].team[i].party[1].valk)) {
                         img2 = valkimgs[j].img;
                     }
-                    if (valkimgs[j].acronym.includes(MABosses[0].team[i].party[2].valk)) {
+                    if (valkimgs[j].acronym.includes(MABosses[bossIndex].team[i].party[2].valk)) {
                         img3 = valkimgs[j].img;
                     }
                 }
                 party += "<tr>";
-                    party += "<td><img src='" + img1 + "'></td>";
-                    party += "<td><img src='" + img2 + "'></td>";
-                    party += "<td><img src='" + img3 + "'></td>";
+                    party += "<td><img class='valkimg-style' src='" + img1 + "'></td>";
+                    party += "<td><img class='valkimg-style' src='" + img2 + "'></td>";
+                    party += "<td><img class='valkimg-style' src='" + img3 + "'></td>";
                 party += "</tr>";
                 party += "<tr>";
-                    party += "<td class='label-style'>" + MABosses[0].team[i].party[0].rank + "</td>";
-                    party += "<td class='label-style'>" + MABosses[0].team[i].party[1].rank + "</td>";
-                    party += "<td class='label-style'>" + MABosses[0].team[i].party[2].rank + "</td>";
+                    party += "<td class='rank-style'>" + MABosses[bossIndex].team[i].party[0].rank + "</td>";
+                    party += "<td class='rank-style'>" + MABosses[bossIndex].team[i].party[1].rank + "</td>";
+                    party += "<td class='rank-style'>" + MABosses[bossIndex].team[i].party[2].rank + "</td>";
                 party += "</tr>";
             party += "</table>";
         party += '</div>';
     }
 
     return party;
+}
+
+function findBossIndex(bossName) {
+    for (var i=0; MABossesLength>i; i++){
+        if (MABosses[i].name.includes(bossName)){
+            bossIndex = i;
+        }
+    }
+    //console.log("bossIndex: " + bossIndex);
+
+    return bossIndex;
 }
